ec92fae454b0bb97002c7bfa3c3ab9565ccc99f3
Tom Christie <tom@tomchristie.com>
2017-02-08 10:27:57 +0000

    Handle unsortable types in 'uniq' helper func.

--- coreschema/utils.py
+++ coreschema/utils.py
@@ -40,17 +40,24 @@ def uniq(container):
     """
     Check if all of a container's elements are unique.
     Successively tries first to rely that the elements are hashable, then
-    falls back on them being sortable.
+    falls back on them being sortable, and finally falls back on brute
+    force.
     """
 
     try:
         return len(set(unbool(i) for i in container)) == len(container)
     except TypeError:
-        pass
-
-    sort = sorted(unbool(i) for i in container)
-    sliced = itertools.islice(sort, 1, None)
-    for i, j in zip(sort, sliced):
-        if i == j:
-            return False
+        try:
+            sort = sorted(unbool(i) for i in container)
+            sliced = itertools.islice(sort, 1, None)
+            for i, j in zip(sort, sliced):
+                if i == j:
+                    return False
+        except (NotImplementedError, TypeError):
+            seen = []
+            for e in container:
+                e = unbool(e)
+                if e in seen:
+                    return False
+                seen.append(e)
     return True
